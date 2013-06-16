f8140357c76ac9081620683598bb1669f6963ad8
Kurt Chen <kurt@kurtchen.com>
2013-06-16 23:23:35 +0800

    modify kindle plugin

--- kindle/init.php
+++ kindle/init.php
@@ -1,50 +1,54 @@
 <?php
 class Kindle extends Plugin {
-	private $link;
-	private $host;
+    private $host;
 
-	function init($host) {
-		$this->link = $host->get_link();
-		$this->host = $host;
+    function init($host) {
+        $this->host = $host;
 
-		$host->add_hook($host::HOOK_ARTICLE_BUTTON, $this);
-	}
+        $host->add_hook($host::HOOK_ARTICLE_BUTTON, $this);
+    }
 
-	function about() {
-		return array(1.1,
-			"Send article to Kindle",
-			"m.s");
-	}
+    function about() {
+        return array(1.0,
+            "Send article to Kindle",
+            "m.s");
+    }
 
-	function get_js() {
-		return file_get_contents(dirname(__FILE__) . "/kindle.js");
-	}
+    function get_js() {
+        return file_get_contents(dirname(__FILE__) . "/kindle.js");
+    }
 
-	function hook_article_button($line) {
-		$article_id = $line["id"];
+    function hook_article_button($line) {
+        $article_id = $line["id"];
 
-		$rv = "<img src=\"plugins/kindle/kindle.png\" class='tagsPic' style=\"cursor : pointer\" onclick=\"shareArticleToKindle($article_id)\" title='".__('Send article to Kindle')."'>";
+        $rv = "<img src=\"plugins/kindle/kindle.png\"
+            class='tagsPic' style=\"cursor : pointer\"
+                onclick=\"shareArticleToKindle($article_id)\"
+                title='".__('Send article to Kindle')."'>";
 
-		return $rv;
-	}
+        return $rv;
+    }
 
-	function getInfo() {
-		$id = db_escape_string($this->link, $_REQUEST['id']);
+    function getInfo() {
+        $id = db_escape_string($_REQUEST['id']);
 
-		$result = db_query($this->link, "SELECT title, link
-				FROM ttrss_entries, ttrss_user_entries
-				WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
+        $result = db_query("SELECT title, link
+            FROM ttrss_entries, ttrss_user_entries
+            WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
 
-		if (db_num_rows($result) != 0) {
-			$title = truncate_string(strip_tags(db_fetch_result($result, 0, 'title')),
-				100, '...');
-			$article_link = db_fetch_result($result, 0, 'link');
-		}
+        if (db_num_rows($result) != 0) {
+            $title = truncate_string(strip_tags(db_fetch_result($result, 0, 'title')),
+                100, '...');
+            $article_link = db_fetch_result($result, 0, 'link');
+        }
 
-		print json_encode(array("title" => $title, "link" => $article_link,
-				"id" => $id));
-	}
+        print json_encode(array("title" => $title, "link" => $article_link,
+            "id" => $id));
+    }
 
+    function api_version() {
+        return 2;
+    }
 
 }
 ?>
--- kindle/kindle.js
+++ kindle/kindle.js
@@ -1,31 +1,31 @@
-	function shareArticleToKindle(id) {
-	try {
-		var query = "?op=pluginhandler&plugin=kindle&method=getInfo&id=" + param_escape(id);
+function shareArticleToKindle(id) {
+    try {
+        var query = "?op=pluginhandler&plugin=kindle&method=getInfo&id=" + param_escape(id);
 
-		console.log(query);
+        console.log(query);
 
-		var d = new Date();
-      var ts = d.getTime();
+        var d = new Date();
+        var ts = d.getTime();
 
-		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_tweet',
-			"status=0,toolbar=0,location=0,width=900,height=600,scrollbars=1,menubar=0");
+        var w = window.open('backend.php?op=backend&method=loading', 'ttrss_kindle',
+                "status=0,toolbar=0,location=0,width=900,height=600,scrollbars=1,menubar=0");
 
-		new Ajax.Request("backend.php",	{
-			parameters: query,
-			onComplete: function(transport) {
-				var ti = JSON.parse(transport.responseText);
+        new Ajax.Request("backend.php",	{
+            parameters: query,
+            onComplete: function(transport) {
+                var ti = JSON.parse(transport.responseText);
 
-				var share_url = "http://fivefilters.org/kindle-it/send.php?url=" +
-					param_escape(ti.link) +
-					"&title=" + param_escape(ti.title);
+                var share_url = "http://fivefilters.org/kindle-it/send.php?url=" +
+            param_escape(ti.link) +
+            "&title=" + param_escape(ti.title);
 
-				w.location.href = share_url;
+        w.location.href = share_url;
 
-			} });
+            } });
 
 
-	} catch (e) {
-		exception_error("tweetArticle", e);
-	}
-	}
+    } catch (e) {
+        exception_error("shareArticleToKindle", e);
+    }
+}
 
